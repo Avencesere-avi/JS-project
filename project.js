d8cee7e94a2c526d40d2a92500f09c70c477857b
Ravi Kumar <raviavi2002@gmail.com>
2023-03-18 11:24:17 +0530

    added (no. of line you want of bet ) and how mmuch you want to bet on your line

--- project.js
+++ project.js
@@ -1,4 +1,7 @@
 
+
+
+
 const prompt = require ("prompt-sync")();
 
  const deposit = () =>{
@@ -27,7 +30,55 @@ const prompt = require ("prompt-sync")();
     
 };
 
+// Enter the no. of lines you bet on (1-3)
+
+ const asking_bet=()=>{
+
+   while(true){
+      const bet = prompt("Which Line you want ot bet on (1-3) ");
+      const numbet = parseInt(bet);
+    
+      if( isNaN(numbet) || numbet<=0 || numbet >3 )
+      {
+       console.log("Try Again , Enter a Valid Number ")
+      }
+    
+      else{
+       return numbet
+      }
+    }
+ }
+
+ // How much you want to bet on your line 
+
+const amountonline =(balance)=>{
 
 
-const xx = deposit();
-console.log(xx);
+    while(true)
+    {
+      const userinputamountofline =  prompt("How much you want to bet on your line " + " : ");
+
+      const filtuserinputamountofline = parseFloat(userinputamountofline)
+ 
+      if(isNaN(filtuserinputamountofline)|| filtuserinputamountofline  > balance || filtuserinputamountofline<0)
+      {
+ 
+       console.log("Try Again , Enter a Valid Amount ")
+ 
+      }
+ 
+      else{
+       return filtuserinputamountofline;
+      }
+ 
+    }
+
+
+
+
+
+
+}
+const balance = deposit();
+const yy = asking_bet();
+const zz = amountonline();
